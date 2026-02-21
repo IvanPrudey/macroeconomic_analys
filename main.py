bfe16ee563aa6672d506ded6af2edf5947c54dcc
IvanPrudey <prudey.ia@yandex.ru>
2026-02-21 22:08:48 +0300

    add_def create_list_of_files_csv

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
 import os
-from pathlib import Path
+import sys
 
-from constants import EXPECTED_HEADERS
+from constants import EXPECTED_HEADERS, DATA_FOLDER
 
 
 def verify_csv_headers(file_path, expected_headers_str):
@@ -17,16 +17,26 @@ def verify_csv_headers(file_path, expected_headers_str):
         return False
 
 
-def main():
-    data_folder = 'data'
-    full_path_data = os.path.abspath(data_folder)
+def create_list_of_files_csv(data_folder):
     data_files_name_list = []
     if os.path.exists(data_folder):
         for filename in os.listdir(data_folder):
             file_path = os.path.join(data_folder, filename)
             if os.path.isfile(file_path) and filename.endswith('.csv'):
                 data_files_name_list.append(filename)
-        print(f'список файлов в папке {full_path_data} - {data_files_name_list}')
+        return data_files_name_list
+    else:
+        print(f'Папка {data_folder} не существует')
+        return data_files_name_list
+
+
+def main():
+    full_path_data = os.path.abspath(DATA_FOLDER)
+    data_files_name_list = create_list_of_files_csv(DATA_FOLDER)
+    if not data_files_name_list:
+        print('Нет файлов данных .csv для обработки. Программа завершена')
+        sys.exit(1)
+    print(f'список файлов в папке data/ {data_files_name_list}')
 
 
 if __name__ == '__main__':
