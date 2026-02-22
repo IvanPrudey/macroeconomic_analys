9e4fa07d8dc3976c653c06b603f7e6734823e84f
IvanPrudey <prudey.ia@yandex.ru>
2026-02-22 11:16:20 +0300

    correct_with_@property_and_@abstractmethod

--- reports/base_report.py
+++ reports/base_report.py
@@ -1,15 +1,20 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List, Dict, Any
 
 
 class Report(ABC):
 
+    @property
+    @abstractmethod
     def name(self) -> str:
         pass
 
+    @property
+    @abstractmethod
     def headers(self) -> List[str]:
         pass
 
+    @abstractmethod
     def calculate(
             self, data: List[Dict[str, Any]]
     ) -> List[Dict[str, Any]]:
