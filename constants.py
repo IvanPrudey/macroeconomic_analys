900e0b147c7dd345b32ed3b7dd5fdf3d1fba0e1c
IvanPrudey <prudey.ia@yandex.ru>
2026-02-22 12:32:47 +0300

    add_AVAILABLE_REPORTS_constant

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
 EXPECTED_HEADERS = 'country,year,gdp,gdp_growth,inflation,unemployment,population,continent'
 DATA_FOLDER = 'data'
+AVAILABLE_REPORTS = {
+    'average-gdp': AverageGDPReport,
+    'some': SomeReport,
+}
