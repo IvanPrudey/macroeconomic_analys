325bd0d10ceef7623e6f72c5961cba5d5aa8afa4
IvanPrudey <prudey.ia@yandex.ru>
2026-02-24 16:08:58 +0300

    add_def test_load_one_file

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,8 @@
-from main import verify_csv_headers
+import os
+from main import (
+    verify_csv_headers,
+    load_data_from_csv_files,
+)
 
 
 class TestVerifyCsvHeaders:
@@ -24,3 +28,34 @@ class TestVerifyCsvHeaders:
             expected_headers_string
         )
         assert result is False
+
+    def test_empty_file_returns_false(
+            self,
+            temp_empty_csv_file,
+            expected_headers_string
+    ):
+        result = verify_csv_headers(
+            temp_empty_csv_file,
+            expected_headers_string
+        )
+        assert result is False
+
+
+class TestLoadDataFromCsvFiles:
+
+    def test_load_one_file(
+            self,
+            temp_csv_with_test_data,
+            expected_headers_string,
+            correct_some_test_data
+    ):
+        folder = os.path.dirname(temp_csv_with_test_data)
+        filename = os.path.basename(temp_csv_with_test_data)
+        result = load_data_from_csv_files(
+            [filename],
+            folder,
+            expected_headers_string
+        )
+        assert isinstance(result, list)
+        assert len(result) == len(correct_some_test_data)
+        assert result[0]['country'] == 'Russia'
